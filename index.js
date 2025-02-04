9ae3df3b231a34cbd245410e7e6a0e6e65dbae9b
Ryan Machando <150363360+ryanmachando@users.noreply.github.com>
2025-02-04 16:31:45 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * This is index.js
+ * Open index.html in your browser to check what
+ * you have to do, adding functions to the end of this
+ * file as necessary.
+ *
+ * NB: all code you write this year should use strict mode, so
+ * we've enabled that by default with the first line of code.
+ */
+
+'use strict';
+
+// add your functions here
